f658d1a500e1f87f836944e92a9c2f7ad92dd5d4
Aaron Burdick <burdick.aaron@gmail.com>
2016-11-29 09:19:57 +0900

    Changes to special offer at the top and location templating. Special offer classes also exclude the blue style from the template and use the proper orange.

--- assets/js/main.js
+++ assets/js/main.js
@@ -272,28 +272,32 @@
 	var priceTemplate = Handlebars.compile(pricingSource);
 	// get the proper info, make it usable by handlebars, and then pass it to the template for compilation
 	$.getJSON('./json/coworking-locations.json', function(data){
+		// Get cheapest location out of all locations
 		var cheapestLocation = data
 			.map(function(location){
-				console.log(location.japan)
 				return {
 					name: location.name,
 					price: location.price,
 					city: location.address.split('<br>')[2].split(' ')[0],
-					japan: location.japan
+					japan: location.japan,
+					url: location.url,
+					phoneNumber: location.phoneNumber
 				}
 			})
 			.filter(function(location){
 				return location.japan
 			})
 			.reduce(function(min, location){
-				var locationPrice = Number(location.price.replace(/円|,/g, ''))
-				var minPrice = Number(min.price.replace(/円|,/g, ''))
-				return Math.min(minPrice, locationPrice) < minPrice ? location : min
+				var locationPrice = Number(location.price.replace(/¥|,/g, ''))
+				var minPrice = Number(min.price.replace(/¥|,/g, ''))
+				return minPrice < locationPrice
+					? min
+					: location
 			})
-		$('#featured-coworking-space').html('<a href="#">'+cheapestLocation.name+'</a>' + ', ' + cheapestLocation.city + ' is only ' + cheapestLocation.price + ' per month!')
+		// Construct a string from the cheapest location and put in tag
+		$('#featured-coworking-space').html('Special offer at <a href="#'+cheapestLocation.url+'" class="smooth">'+cheapestLocation.name+', '+cheapestLocation.city+'</a>.<br>First 2 months at half price ' + cheapestLocation.price + '/month!<br>No further obligation! Call <span class="phone-number">+81 '+cheapestLocation.phoneNumber+'</span>')
 
 		var context = {};
-		var exampleCity = "Level 1 Yusen Building<br>2-3-2 Marunouchi, Chiyoda-ku<br>Tokyo 100-0005 Japan";
 		context.locations = data;
 		
 		var html = template(context);
